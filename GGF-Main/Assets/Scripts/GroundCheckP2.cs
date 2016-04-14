df639613fcd49eb3e113f1044e91911fd02c2270
Ghostiex <joel.o.l.nilsson@gmail.com>
2016-04-14 17:50:15 +0200

    Adding more från Oscar

---
+++ GGF-Main/Assets/Scripts/GroundCheckP2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheckP2 : MonoBehaviour
+{
+    private Player2 player2;
+
+    void Start()
+    {
+        player2 = gameObject.GetComponentInParent<Player2>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        player2.grounded = true;
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        player2.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        player2.grounded = false;
+    }
+}
--- GGF-Main/Assets/Scripts/Player.cs
+++ GGF-Main/Assets/Scripts/Player.cs
@@ -1,34 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public float maxSpeed;
     public float speed;
     public float jumpPower;
 
     public bool grounded;
+    public int doubleJump;
+    private bool HasDoubleJumped;
     private bool jumpState;
     private bool oldJumpState;
+    public bool paralyzed;
+    public bool autoAimed;
 
     private Rigidbody2D rbPlayer;
+    public Player2 player2;
     private Animator animator;
 
-	void Start ()
+    public Transform firePoint;
+    public GameObject laserBullet;
+    public bool shoot;
+    public int shots;
+
+    //Timers 
+    public float paralyzedReset;
+    public float paralyzedTimer;
+    public float speedBoostTimer = 0;
+
+    void Start()
     {
+        paralyzedReset = 0.6f;
+        paralyzedTimer = paralyzedReset;
+
         maxSpeed = 6f;
         speed = 50f;
         jumpPower = 250f;
+        shots = 100;
+
+        doubleJump = 0;
 
         rbPlayer = gameObject.GetComponent<Rigidbody2D>();
 
         animator = gameObject.GetComponent<Animator>();
-	}
-	
-	void Update ()
+
+        player2 = player2.GetComponent<Player2>();
+    }
+
+    void Update()
     {
-        oldJumpState = jumpState;
-        jumpState = Input.GetButton("Jump");
+
+        if (speedBoostTimer > 0)
+        {
+            speedBoostTimer -= Time.deltaTime;
+            maxSpeed = 10f;
+        }
+
+        if (speedBoostTimer <= 0)
+        {
+            maxSpeed = 6f;
+        }
 
         animator.SetBool("Grounded", grounded);
         animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
@@ -43,17 +76,61 @@ public class Player : MonoBehaviour {
             transform.localScale = new Vector3(1, 1, 1);
         }
 
+        oldJumpState = jumpState;
+        jumpState = Input.GetButton("Jump");
+
+
+        //DoubleJump
+        if (jumpState && !oldJumpState && !grounded && (doubleJump > 0) && !HasDoubleJumped)
+        {
+            HasDoubleJumped = true;
+
+            rbPlayer.AddForce(Vector2.up * jumpPower);
+
+            doubleJump -= 1;
+        }
+
+
+        //SingleJump
         if (jumpState && !oldJumpState && grounded)
         {
+            HasDoubleJumped = false;
+
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, 0);
+
             rbPlayer.AddForce(Vector2.up * jumpPower);
         }
-	}
+
+        //Shoot
+        if (Input.GetButtonDown("Fire1") && shots > 0)
+        {
+            shoot = true;
+            Instantiate(laserBullet, firePoint.position, firePoint.rotation);
+            shots -= 1;
+        }
+    }
 
     void FixedUpdate()
     {
+        if (paralyzed)
+        {
+            paralyzedTimer -= Time.deltaTime;
+            rbPlayer.velocity = new Vector2(0.0f, rbPlayer.velocity.y);
+            //rbPlayer.AddForce(Vector2.left * jumpPower);
+            if (paralyzedTimer < 0)
+            {
+                paralyzed = false;
+                paralyzedTimer = paralyzedReset;
+            }
+        }
+
         float h = Input.GetAxis("Horizontal");
 
-        rbPlayer.AddForce((Vector2.right * speed) * h);
+        if (!paralyzed)
+        {
+            rbPlayer.AddForce((Vector2.right * speed) * h);
+        }
+
 
         if (rbPlayer.velocity.x > maxSpeed)
         {
@@ -65,4 +142,26 @@ public class Player : MonoBehaviour {
             rbPlayer.velocity = new Vector2(-maxSpeed, rbPlayer.velocity.y);
         }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DoubleJump"))
+        {
+            other.gameObject.SetActive(false);
+            doubleJump += 1;
+        }
+
+        if (other.gameObject.CompareTag("SpeedBoost"))
+        {
+            other.gameObject.SetActive(false);
+            speedBoostTimer = 0.8f;
+        }
+
+        if (other.gameObject.CompareTag("Ammo"))
+        {
+            other.gameObject.SetActive(false);
+            shots += 1;
+        }
+    }
+
 }
