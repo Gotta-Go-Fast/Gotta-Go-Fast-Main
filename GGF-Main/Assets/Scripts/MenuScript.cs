cb6d23d8ddeb08d9e2a9234eac2f03643e9ba437
EInour <nilsson.o.90@gmail.com>
2016-04-25 20:30:41 +0200

    Övergång mellan Menyer i samma Scene

---
+++ GGF-Main/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Button startGame;
+
+    public Canvas mainCanvas;
+    public Canvas charCanvas;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        startGame = startGame.GetComponent<Button>();
+
+        mainCanvas = mainCanvas.GetComponent<Canvas>();
+
+        charCanvas = charCanvas.GetComponent<Canvas>();
+
+
+        mainCanvas.enabled = true;
+        charCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+    public void StartPress()
+    {
+        mainCanvas.enabled = false;
+        charCanvas.enabled = true;
+    }
+
+
+}
