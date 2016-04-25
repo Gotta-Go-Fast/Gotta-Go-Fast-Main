a43059c309a90bd0a8332e4400d68d610386326d
EInour <nilsson.o.90@gmail.com>
2016-04-26 01:13:36 +0200

    Menu fix

--- GGF-Main/Assets/Scripts/MenuScript.cs
+++ GGF-Main/Assets/Scripts/MenuScript.cs
@@ -4,38 +4,139 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
+
+    // MainMenu Buttons
     public Button startGame;
+    public Button options;
+    public Button quitGame;
 
-    public Canvas mainCanvas;
-    public Canvas charCanvas;
+    // OptionsMenu Buttons
+    public Button toggleSound;
+    public Button optionsBack;
+
+    // CharacterSelection Buttons
+    public Button levelSelection;
+    public Button characterBack;
+
+    // LevelSelection Buttons
+    public Button play;
+    public Button levelBack;
+
+    // Canvases
+    public Canvas mainMenuCanvas;
+    public Canvas optionsCanvas;
+    public Canvas characterMenuCanvas;
+    public Canvas levelMenuCanvas;
+
+    // Music
+    public AudioSource backgroundMusic;
+    public bool mute;
 
 
-
-    // Use this for initialization
     void Start ()
     {
+        // MainMenu Buttons
         startGame = startGame.GetComponent<Button>();
+        options = options.GetComponent<Button>();
+        quitGame = quitGame.GetComponent<Button>();
 
-        mainCanvas = mainCanvas.GetComponent<Canvas>();
+        // OptionsMenu Buttons
+        toggleSound = toggleSound.GetComponent<Button>();
+        optionsBack = optionsBack.GetComponent<Button>();
 
-        charCanvas = charCanvas.GetComponent<Canvas>();
+        // CharacterSelection Buttons
+        levelSelection = levelSelection.GetComponent<Button>();
+        characterBack = characterBack.GetComponent<Button>();
 
+        // LevelSelection Buttons
+        play = play.GetComponent<Button>();
+        levelBack = levelBack.GetComponent<Button>();
 
-        mainCanvas.enabled = true;
-        charCanvas.enabled = false;
+        // Canvases
+        mainMenuCanvas = mainMenuCanvas.GetComponent<Canvas>();
+        optionsCanvas = optionsCanvas.GetComponent<Canvas>();
+        characterMenuCanvas = characterMenuCanvas.GetComponent<Canvas>();
+        levelMenuCanvas = levelMenuCanvas.GetComponent<Canvas>();
+
+        // Music
+        backgroundMusic = backgroundMusic.GetComponent<AudioSource>();
+        mute = false;
+
+        mainMenuCanvas.enabled = true;
+        optionsCanvas.enabled = false;
+        characterMenuCanvas.enabled = false;
+        levelMenuCanvas.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update ()
-    {
 
-	}
+    // Main Menu
 
-    public void StartPress()
+    public void StartGame()
     {
-        mainCanvas.enabled = false;
-        charCanvas.enabled = true;
+        mainMenuCanvas.enabled = false;
+        characterMenuCanvas.enabled = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        mainMenuCanvas.enabled = false;
+        optionsCanvas.enabled = true;
     }
 
 
+    // Options Menu
+
+    public void ToggleSound()
+    {
+        if (mute)
+        {
+            mute = false;
+            backgroundMusic.Play();
+        }
+        else
+        {
+            mute = true;
+            backgroundMusic.Stop();
+        }
+    }
+
+    public void OptionsBack()
+    {
+        optionsCanvas.enabled = false;
+        mainMenuCanvas.enabled = true;
+    }
+
+
+    // Character Selection Menu
+
+    public void LevelSelection()
+    {
+        characterMenuCanvas.enabled = false;
+        levelMenuCanvas.enabled = true;
+    }
+
+    public void CharacterSelectionBack()
+    {
+        characterMenuCanvas.enabled = false;
+        mainMenuCanvas.enabled = true;
+    }
+
+
+    // Level Selection Menu
+
+    public void Play()
+    {
+
+    }
+
+    public void LevelSelectionBack()
+    {
+        levelMenuCanvas.enabled = false;
+        characterMenuCanvas.enabled = true;
+    }
 }
