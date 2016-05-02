ccfe1207a7841089ccf0916fad44aa64bb9ccf48
maxwalhagen <maxwalhagen1@gmail.com>
2016-05-02 15:12:06 +0200

    Character selection

---
+++ GGF-Main/Assets/Scripts/SpawnPointScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPointScript : MonoBehaviour {
+
+	void Awake()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag ("Player1");
+		player = player;
+		//player.transform.position=
+	}
+}
